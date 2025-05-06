5f1260693a860504493a1bc4e35d9d0920f7a9e2
EdoardoLoIacono06 <e.loiacono2006@gmail.com>
2025-05-06 17:00:11 +0200

    Migliorata la grafica per dispositivi android, migliorato la gestione dellastanze e aggiunta la grafica per la gestione del delay nell'attivazione di un allarme

--- src/app/pages/allarmi/allarmi.component.ts
+++ src/app/pages/allarmi/allarmi.component.ts
@@ -1,18 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { AllarmeComponent } from 'src/app/shared/allarme/allarme.component';
 
 @Component({
   selector: 'app-allarmi',
   templateUrl: './allarmi.component.html',
   styleUrls: ['./allarmi.component.scss'],
-  imports: [IonicModule, RouterLink, RouterLinkActive,AllarmeComponent]
+  imports: [IonicModule, RouterLink, RouterLinkActive,AllarmeComponent,CommonModule,FormsModule]
 })
 export class AllarmiComponent  implements OnInit {
+  delay: number = 0;
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {}
 
+  saveDelay(){
+    this.delayToast(`Delay impostato a ${this.delay} secondi`)
+  }
+
+  async delayToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'success'
+    });
+    toast.present();
+  }
 }
